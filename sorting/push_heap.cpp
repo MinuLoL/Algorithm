ff613dd838111d6e0c52747b57adc5a5c77ace68
MinuLoL <alsdn4837@naver.com>
2020-07-18 17:03:45 +0900

    add sorting algorithm

---
+++ sorting/push_heap.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	vector<int> v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.push_back(40);
+	v.push_back(50);
+	v.push_back(60);
+	
+	make_heap(v.begin(),v.end());
+	cout<<"v[힙 생성]: ";
+	for(vector<int>::size_type i=0;i<v.size();++i)
+		cout<<v[i]<<" ";
+	cout<<endl;
+	
+	v.push_back(75);
+	cout<<"순차열에 35추가: ";
+	for(vector<int>::size_type i=0;i<v.size();++i)
+		cout<<v[i]<<" ";
+	cout<<endl;
+	
+	push_heap(v.begin(),v.end());
+	cout<<"v[힙 추가]연산 수행: ";
+	for(vector<int>::size_type i=0;i<v.size();++i)
+		cout<<v[i]<<" ";
+	cout<<endl;
+	return 0;
+}
---
+++ sorting/stable_sort.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool Greater(int left,int right)
+{
+	return left>right;
+}
+
+int main()
+{
+	vector<int> v;
+	v.push_back(30);
+	v.push_back(50); 
+	v.push_back(30);
+	v.push_back(20);
+	v.push_back(40);
+	v.push_back(10);
+	v.push_back(40);
+	
+	cout<<"v[정렬 전]: ";
+	for(vector<int>::size_type i=0;i<v.size();++i)
+		cout<<v[i]<<" ";
+	cout<<endl;
+	
+	stable_sort(v.begin(),v.end());
+	cout<<"v[정렬less]: ";
+	for(vector<int>::size_type i=0;i<v.size();++i)
+		cout<<v[i]<<" ";
+	cout<<endl;
+	stable_sort(v.begin(),v.end(),greater<int>());
+	cout<<"v[정렬greater]: ";
+	for(vector<int>::size_type i=0;i<v.size();++i)
+		cout<<v[i]<<" ";
+	cout<<endl;
+	
+	return 0;
+	
+}
